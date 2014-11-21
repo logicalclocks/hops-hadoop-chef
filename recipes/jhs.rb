9c8e10977127de25dd44ab86bfb56340ac5413d4
Jim Dowling <jdowling@kth.se>
2014-11-21 10:14:27 +0100

    jhs mapped to apache version

--- recipes/jhs.rb
+++ recipes/jhs.rb
@@ -19,36 +19,43 @@ service yarn_command do
   action :nothing
 end
 
+
+tmp_dirs   = ["/mr-history", node[:hadoop][:jhs][:inter_dir], node[:hadoop][:jhs][:done_dir], "/tmp"]
+
+ for d in tmp_dirs
+   Chef::Log.info "Creating hdfs directory: #{d}"
+   hadoop_hdfs_directory d do
+    action :create
+    mode "1777"
+   end
+ end
+
+node.normal[:mr][:dirs] = [node[:hadoop][:mr][:staging_dir], node[:hadoop][:mr][:tmp_dir]]
+ for d in node[:mr][:dirs]
+   Chef::Log.info "Creating hdfs directory: #{d}"
+   hadoop_hdfs_directory d do
+    action :create
+    mode "0775"
+   end
+ end
+
 template "/etc/init.d/#{yarn_command}" do
   source "#{yarn_command}.erb"
-  owner node[:hadoop][:yarn][:user]
+  owner node[:hdfs][:user]
   group node[:hadoop][:group]
   mode 0754
   notifies :enable, resources(:service => yarn_command)
   notifies :restart, resources(:service => yarn_command)
 end
 
-user node[:hadoop][:mr][:user] do
-  supports :manage_home => true
-  home "/home/#{node[:hadoop][:mr][:user]}"
-  action :create
-  system true
-  shell "/bin/bash"
-end
-
-group node[:hadoop][:group] do
-  action :modify
-  members node[:hadoop][:mr][:user]
-  append true
-end
-
-kagent_config yarn_command do
-  service "MAP_REDUCE"
-  start_script "#{node[:hadoop][:home]}/sbin/root-start-#{yarn_service}.sh"
-  stop_script "#{node[:hadoop][:home]}/sbin/stop-#{yarn_service}.sh"
-  log_file "#{node[:hadoop][:logs_dir]}/yarn-#{node[:hdfs][:user]}-#{yarn_command}-#{node['hostname']}.log"
-#  pid_file "#{node[:hadoop][:logs_dir]}/yarn-#{node[:hdfs][:user]}-#{yarn_command}.pid"
-  pid_file "/tmp/mapred-#{node[:hdfs][:user]}-#{yarn_command}.pid"
-  config_file "#{node[:hadoop][:conf_dir]}/mapred-site.xml"
-  web_port node[:hadoop]["#{yarn_service}"][:http_port]
+if node[:kagent][:enabled] == "true" 
+  kagent_config yarn_command do
+    service "MAP_REDUCE"
+    start_script "#{node[:hadoop][:home]}/sbin/root-start-#{yarn_service}.sh"
+    stop_script "#{node[:hadoop][:home]}/sbin/stop-#{yarn_service}.sh"
+    log_file "#{node[:hadoop][:logs_dir]}/yarn-#{node[:hdfs][:user]}-#{yarn_command}-#{node['hostname']}.log"
+    pid_file "/tmp/mapred-#{node[:hdfs][:user]}-#{yarn_command}.pid"
+    config_file "#{node[:hadoop][:conf_dir]}/mapred-site.xml"
+    web_port node[:hadoop]["#{yarn_service}"][:http_port]
+  end
 end
