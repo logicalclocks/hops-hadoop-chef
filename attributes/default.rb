b8da0406bf93a2aacc550c3fe477b928766bab52
Jim Dowling <jdowling@sics.se>
2015-01-08 13:06:25 +0100

    updates

--- attributes/default.rb
+++ attributes/default.rb
@@ -11,10 +11,12 @@ default[:hadoop][:storage_type]            = "clusterj"
 default[:hadoop][:leader_check_interval_ms]= 1000
 default[:hadoop][:missed_hb]               = 1
 default[:hadoop][:repl]                    = 1
-default[:hadoop][:db]                      = "hops"
+node.normal[:hadoop][:db]                  = "hops"
 default[:hadoop][:max_retries]             = 0
 
 # set the location of libndbclient.so. set-env.sh sets LD_LIBRARY_PATH to find this library.
 default[:ndb][:libndb]                     = "#{default[:mysql][:version_dir]}/lib"
 default[:mysql][:port]                     = default[:ndb][:mysql_port]
 default[:hadoop][:mysql_url]               = "jdbc:mysql://#{default[:ndb][:mysql_ip]}:#{default[:ndb][:mysql_port]}/"
+
+default[:hops][:log_level]                 = "DEBUG"
