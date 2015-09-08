2d524c2242c33a61e4b0bc6342fe553091720ea6
Jim Dowling <jdowling@kth.se>
2015-09-08 15:40:04 +0200

    seif's updates

--- providers/ndb.rb
+++ providers/ndb.rb
@@ -16,7 +16,7 @@ new_resource.updated_by_last_action(false)
     code <<-EOF
     set -e
     #{node[:ndb][:scripts_dir]}/mysql-client.sh -e \"CREATE DATABASE IF NOT EXISTS #{node[:hadoop][:db]}\"
-    #{node[:ndb][:scripts_dir]}/mysql-client.sh #{node[:hadoop][:db]} < #{Chef::Config[:file_cache_path]}/hops.sql
+    #{node[:ndb][:scripts_dir]}/mysql-client.sh #{node[:hadoop][:db]} < "#{node[:hadoop][:conf_dir]}/hops.sql"
     EOF
     new_resource.updated_by_last_action(true)
     not_if "#{node[:ndb][:scripts_dir]}/mysql-client.sh #{node[:hadoop][:db]} -e \"show create table hdfs_block_infos;\""
