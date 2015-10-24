3337a5b7540f3b2fec780f1575cd4975b8bdff11
Jim Dowling <jdowling@sics.se>
2015-10-24 22:10:17 +0200

    updates

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -25,19 +25,19 @@ describe service('JobHistoryServer') do
   it { should be_running   }
 end 
 
-describe command("/var/lib/mysql-cluster/ndb/scripts/mysql-client.sh -e \"show databases\"") do
+describe command("/tmp/mysql-cluster/ndb/scripts/mysql-client.sh -e \"show databases\"") do
   its (:stdout) { should match /hops/ }
 end
 
-describe command("/var/lib/mysql-cluster/ndb/scripts/mgm-client.sh -e \"show\"") do
+describe command("/tmp/mysql-cluster/ndb/scripts/mgm-client.sh -e \"show\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -mkdir /hops\"") do
+describe command("su hdfs -l -c \"/tmp/hadoop/bin/hdfs dfs -mkdir /hops\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -ls /\"") do
+describe command("su hdfs -l -c \"/tmp/hadoop/bin/hdfs dfs -ls /\"") do
   its (:stdout) { should match /hops/ }
 end
 
@@ -45,11 +45,11 @@ describe command("su hdfs -l -c \"echo 'test data' > /tmp/hopsie\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -copyFromLocal /tmp/hopsie /hops\"") do
+describe command("su hdfs -l -c \"/tmp/hadoop/bin/hdfs dfs -copyFromLocal /tmp/hopsie /hops\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
+describe command("su yarn -l -c \"/tmp/hadoop/bin/yarn jar /tmp/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
   its (:stdout) { should match /Estimated value of Pi is/ }
 end
 
