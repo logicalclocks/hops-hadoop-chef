c48a6923fc50f385ad99375b7b92c5ab1d2264bd
%github_username% <jdowling@sics.se>
2016-02-09 13:26:19 +0100

    Fixed type bug in purge recipe

--- recipes/purge.rb
+++ recipes/purge.rb
@@ -4,12 +4,12 @@ include_recipe "hadoop::purge"
 
 link "#{node[:hadoop][:dir]}/ndb-hops" do
   action :delete
-  ignore_failure :true
+  ignore_failure true
 end
 
 directory "#{node[:hadoop][:dir]}/ndb-hops-#{node[:hadoop][:version]}-#{node[:ndb][:version]}" do
   recursive true
   action :delete
-  ignore_failure :true
+  ignore_failure true
 end
 
