aee95ebe21d366a07a88f4ad440d10d392b5590b
Jim Dowling <jdowling@sics.se>
2016-02-24 00:33:29 +0100

    backed out gautier's yarn-site for now

--- recipes/rm.rb
+++ recipes/rm.rb
@@ -32,28 +32,28 @@ end
 
 
 
-container_executor="org.apache.hadoop.yarn.server.nodemanager.DefaultContainerExecutor"
-if node[:hadoop][:cgroups].eql? "true" 
-  container_executor="org.apache.hadoop.yarn.server.nodemanager.LinuxContainerExecutor"
-end
+# container_executor="org.apache.hadoop.yarn.server.nodemanager.DefaultContainerExecutor"
+# if node[:hadoop][:cgroups].eql? "true" 
+#   container_executor="org.apache.hadoop.yarn.server.nodemanager.LinuxContainerExecutor"
+# end
 
-file "#{node[:hadoop][:home]}/etc/hadoop/yarn-site.xml" do 
-  owner node[:hdfs][:user]
-  action :delete
-end
+# file "#{node[:hadoop][:home]}/etc/hadoop/yarn-site.xml" do 
+#   owner node[:hdfs][:user]
+#   action :delete
+# end
 
-template "#{node[:hadoop][:home]}/etc/hadoop/yarn-site.xml" do
-  source "yarn-site.xml.erb"
-  owner node[:hadoop][:yarn][:user]
-  group node[:hadoop][:group]
-  mode "666"
-  variables({
-              :rm_private_ip => rm_dest_ip,
-              :rm_public_ip => rm_public_ip,
-              :available_mem_mb => node[:hadoop][:yarn][:nm][:memory_mbs],
-              :my_public_ip => my_public_ip,
-              :my_private_ip => my_ip,
-              :container_executor => container_executor
-            })
-  action :create_if_missing
-end
+# template "#{node[:hadoop][:home]}/etc/hadoop/yarn-site.xml" do
+#   source "yarn-site.xml.erb"
+#   owner node[:hadoop][:yarn][:user]
+#   group node[:hadoop][:group]
+#   mode "666"
+#   variables({
+#               :rm_private_ip => rm_dest_ip,
+#               :rm_public_ip => rm_public_ip,
+#               :available_mem_mb => node[:hadoop][:yarn][:nm][:memory_mbs],
+#               :my_public_ip => my_public_ip,
+#               :my_private_ip => my_ip,
+#               :container_executor => container_executor
+#             })
+#   action :create_if_missing
+# end
