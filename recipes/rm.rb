8eb64bc0d03c6ec0562952bf84c4f175f0a5f8fd
%github_username% <jdowling@sics.se>
2016-02-23 13:57:59 +0100

    fixes to rm and rt config files

--- recipes/rm.rb
+++ recipes/rm.rb
@@ -1,2 +1,28 @@
 include_recipe "hops::wrap"
 include_recipe "hadoop::rm"
+
+
+ndb_connectstring()
+
+template "#{node[:hadoop][:home]}/etc/hadoop/RM_EventAPIConfig.ini" do 
+  source "RM_EventAPIConfig.ini.erb"
+  owner node[:hdfs][:user]
+  group node[:hadoop][:group]
+  mode "755"
+  variables({
+              :ndb_connectstring => node[:ndb][:connectstring]
+            })
+end
+
+template "#{node[:hadoop][:home]}/etc/hadoop/RT_EventAPIConfig.ini" do 
+  source "RT_EventAPIConfig.ini.erb"
+  owner node[:hdfs][:user]
+  group node[:hadoop][:group]
+  mode "755"
+  variables({
+              :ndb_connectstring => node[:ndb][:connectstring]
+            })
+end
+
+
+
