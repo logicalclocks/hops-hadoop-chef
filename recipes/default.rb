93c4623ceb2f28aa7042ebc2e425a2b2b4f71a35
%github_username% <jdowling@sics.se>
2016-02-23 09:09:52 +0100

    create yarn-site.xml file for hops

--- recipes/default.rb
+++ recipes/default.rb
@@ -14,7 +14,7 @@ my_ip = my_private_ip()
 my_public_ip = my_public_ip()
 rm_private_ip = private_recipe_ip("hops","rm")
 rm_public_ip = public_recipe_ip("hops","rm")
-
+rm_dest_ip = rm_private_ip
 
 include_recipe "hops::wrap"
 include_recipe "hadoop::default"
@@ -83,3 +83,31 @@ template "#{node[:hadoop][:conf_dir]}/erasure-coding-site.xml" do
   group node[:hadoop][:group]
   mode "755"
 end
+
+file "#{node[:hadoop][:home]}/etc/hadoop/yarn-site.xml" do 
+  owner node[:hdfs][:user]
+  action :delete
+end
+
+container_executor="org.apache.hadoop.yarn.server.nodemanager.DefaultContainerExecutor"
+if node[:hadoop][:cgroups].eql? "true" 
+  container_executor="org.apache.hadoop.yarn.server.nodemanager.LinuxContainerExecutor"
+end
+
+
+template "#{node[:hadoop][:home]}/etc/hadoop/yarn-site.xml" do
+  source "yarn-site.xml.erb"
+  owner node[:hadoop][:yarn][:user]
+  group node[:hadoop][:group]
+  mode "666"
+  variables({
+              :rm_private_ip => rm_dest_ip,
+              :rm_public_ip => rm_public_ip,
+              :available_mem_mb => node[:hadoop][:yarn][:nm][:memory_mbs],
+              :my_public_ip => my_public_ip,
+              :my_private_ip => my_ip,
+              :container_executor => container_executor
+            })
+  action :create_if_missing
+end
+
