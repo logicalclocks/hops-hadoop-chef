ec8e4b322fbb0bad9364ab0faa570b3c74cca2c0
Jim Dowling <jdowling@sics.se>
2016-03-26 16:19:46 +0100

    added params from apache_hadoop

--- metadata.rb
+++ metadata.rb
@@ -28,28 +28,8 @@ depends 'apache_hadoop'
   supports os
 end
 
-#attribute "apache_hadoop/version",
-#:description => "Hop version number",
-#:type => 'string'
-
-attribute "apache_hadoop/hdfs/user",
-:description => "Username to run hdfs as",
-:type => 'string'
-
-attribute "apache_hadoop/yarn/user",
-:description => "Username to run yarn as",
-:type => 'string'
-
 attribute "hops/yarn/rm_heartbeat",
 :description => "NodeManager heartbeat timeout",
-:type => 'string'
-
-# attribute "apache_hadoop/rm/scheduler_class",
-# :description => "Java Classname for the Yarn scheduler (fifo, capacity, fair)",
-# :type => 'string'
-
-attribute "apache_hadoop/mr/user",
-:description => "Username to run MapReduce as",
 :type => 'string'
 
 attribute "mysql/user",
@@ -61,10 +41,6 @@ attribute "mysql/password",
 :description => "MySql server Password",
 :type => 'string',
 :required => "required"
-
-attribute "apache_hadoop/format",
-:description => "Format HDFS: 'hdfs namenode -format'",
-:type => 'string'
 
 attribute "hops/use_hopsworks",
 :description => "'true' or 'false' - true to enable HopsWorks support",
@@ -107,6 +83,111 @@ attribute "hops/use_systemd",
 :description => "Use systemd startup scripts, default 'false'",
 :type => "string"
 
+
+#
+# wrapper parameters 
+#
+
+attribute "apache_hadoop/yarn/nm/memory_mbs",
+:display_name => "Apache_Hadoop NodeManager Memory in MB",
+:type => 'string'
+
+attribute "apache_hadoop/yarn/vcores",
+:display_name => "Apache_Hadoop NodeManager Number of Virtual Cores",
+:type => 'string'
+
+attribute "apache_hadoop/yarn/max_vcores",
+:display_name => "Hadoop NodeManager Maximum Virtual Cores per container",
+:type => 'string'
+
+attribute "apache_hadoop/version",
+:display_name => "Hadoop version",
+:description => "Version of apache_hadoop",
+:type => 'string'
+
+attribute "apache_hadoop/num_replicas",
+:display_name => "HDFS replication factor",
+:description => "Number of replicates for each file stored in HDFS",
+:type => 'string'
+
+attribute "apache_hadoop/container_cleanup_delay_sec",
+:display_name => "Cleanup Delay (s)",
+:description => "The number of seconds container data is retained after termination",
+:type => 'string'
+
+attribute "apache_hadoop/yarn/user",
+:display_name => "Username to run yarn as",
+:description => "Username to run yarn as",
+:type => 'string'
+
+attribute "apache_hadoop/mr/user",
+:display_name => "Username to run mapReduce as",
+:description => "Username to run mapReduce as",
+:type => 'string'
+
+attribute "apache_hadoop/hdfs/user",
+:display_name => "Username to run hdfs as",
+:description => "Username to run hdfs as",
+:type => 'string'
+
+attribute "apache_hadoop/format",
+:display_name => "Format HDFS",
+:description => "Format HDFS, Run 'hdfs namenode -format",
+:type => 'string'
+
+attribute "apache_hadoop/tmp_dir",
+:display_name => "Hadoop Temp Dir",
+:description => "The directory in which Hadoop stores temporary data, including container data",
+:type => 'string'
+
 attribute "apache_hadoop/data_dir",
-:description => "Directory to store actual hadoop data",
-:type => "string"
+:display_name => "HDFS Data Dir",
+:description => "The directory in which Hadoop's DataNodes store their data",
+:type => 'string'
+
+attribute "apache_hadoop/yarn/nodemanager_hb_ms",
+:description => "Heartbeat Interval for NodeManager->ResourceManager in ms",
+:type => 'string'
+
+attribute "apache_hadoop/container_cleanup_delay_sec",
+:display_name => "Cleanup Delay (s)",
+:description => "The number of seconds container data is retained after termination",
+:type => 'string'
+
+attribute "apache_hadoop/rm/scheduler_class",
+:display_name => "YARN scheduler class",
+:description => "Java Classname for the Yarn scheduler (fifo, capacity, fair)",
+:type => 'string'
+
+attribute "apache_hadoop/rm/scheduler_capacity/calculator_class",
+:display_name => "YARN resource calculator class",
+:description => "Switch to DominantResourseCalculator for multiple resource scheduling",
+:type => 'string'
+
+attribute "apache_hadoop/user_envs",
+:description => "Update the PATH environment variable for the hdfs and yarn users to include hadoop/bin in the PATH ",
+:type => 'string'
+
+attribute "apache_hadoop/logging_level",
+:description => "Log levels are: TRACE, DEBUG, INFO, WARN",
+:type => 'string'
+
+attribute "apache_hadoop/nn/heap_size",
+:description => "Size of the NameNode heap in MBs",
+:type => 'string'
+
+attribute "apache_hadoop/nn/direct_memory_size",
+:description => "Size of the direct memory size for the NameNode in MBs",
+:type => 'string'
+
+attribute "apache_hadoop/ha_enabled",
+:description => "'true' to enable HA, else 'false'",
+:type => 'string'
+
+attribute "apache_hadoop/yarn/rt",
+:display_name => "Hadoop Resource Tracker enabled on this nodegroup",
+:type => 'string'
+
+attribute "apache_hadoop/dir",
+:display_name => "Hadoop installation directory",
+:type => 'string'
