edb885fd52fbdd64e39aa85f787e20a01cc97e47
Jim Dowling <jdowling@sics.se>
2016-08-31 22:08:29 +0200

    version is 0.1.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jdowling@kth.se"
 license          "GPL 2.0"
 description      'Installs/Configures HOPS distribution'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.2"
+version          "0.1.1"
 source_url       "https://github.com/hopshadoop/hops-hadoop-chef"
 
 
