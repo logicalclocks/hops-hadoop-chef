0f1a7a3ad0ca4f52c710a087e44f84bdbab78096
karamel <jdowling@sics.se>
2016-09-23 09:46:40 +0200

    fix serverspec test

--- test/integration/default/serverspec/localhost/services_spec.rb
+++ test/integration/default/serverspec/localhost/services_spec.rb
@@ -25,15 +25,15 @@ describe service('resourcemanager') do
   it { should be_running   }
 end
 
-describe command("/tmp/mysql-cluster/ndb/scripts/mysql-client.sh -e \"show databases\"") do
+describe command("/var/lib/mysql-cluster/ndb/scripts/mysql-client.sh -e \"show databases\"") do
   its (:stdout) { should match /mysql/ }
 end
 
-describe command("su hdfs -l -c \"/tmp/hadoop/bin/hdfs dfs -mkdir /hops\"") do
+describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -mkdir /hops\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su hdfs -l -c \"/tmp/hadoop/bin/hdfs dfs -ls /\"") do
+describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -ls /\"") do
   its (:stdout) { should match /hops/ }
 end
 
@@ -41,11 +41,11 @@ describe command("su hdfs -l -c \"echo 'test data' > /tmp/hopsie\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su hdfs -l -c \"/tmp/hadoop/bin/hdfs dfs -copyFromLocal /tmp/hopsie /hops\"") do
+describe command("su hdfs -l -c \"/srv/hadoop/bin/hdfs dfs -copyFromLocal /tmp/hopsie /hops\"") do
   its(:exit_status) { should eq 0 }
 end
 
-describe command("su yarn -l -c \"/tmp/hadoop/bin/yarn jar /tmp/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
+describe command("su yarn -l -c \"/srv/hadoop/bin/yarn jar /srv/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-examples-2.4.0.jar pi 1 1000 \"") do
   its (:stdout) { should match /Estimated value of Pi is/ }
 end
 
