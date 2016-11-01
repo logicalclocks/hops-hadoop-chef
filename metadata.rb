4880bf1993c9f437c7eac0eafdc330b4e65a2f1a
Gautier Berthou <gautier.berthou@gmail.com>
2016-11-01 11:36:03 +0100

    add possibility to set where the yarn users logs are writen

--- metadata.rb
+++ metadata.rb
@@ -147,6 +147,10 @@ attribute "apache_hadoop/tmp_dir",
           :description => "The directory in which Hadoop stores temporary data, including container data",
           :type => 'string'
 
+attribute "apache_hadoop/nodemanager/log-dirs",
+          :description => "The directory in which yarn node manager store containers logs",
+          :type => 'string'
+
 attribute "apache_hadoop/data_dir",
           :description => "The directory in which Hadoop's DataNodes store their data",
           :type => 'string'
