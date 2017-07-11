220dff664355f1af706c4e4ece7562040b93db96
Jim Dowling <jdowling@sics.se>
2017-07-11 07:20:49 +0100

    make format_nn a new recipe, runs before nn recipe

--- recipes/nn.rb
+++ recipes/nn.rb
@@ -98,40 +98,6 @@ include_recipe "hops::default"
 # end
 
 
-for script in node.hops.nn.scripts
-  template "#{node.hops.home}/sbin/#{script}" do
-    source "#{script}.erb"
-    owner node.hops.hdfs.user
-    group node.hops.group
-    mode 0775
-  end
-end 
-
-Chef::Log.info "NameNode format option: #{node.hops.nn.format_options}"
-
-template "#{node.hops.home}/sbin/format-nn.sh" do
-  source "format-nn.sh.erb"
-  owner node.hops.hdfs.user
-  group node.hops.group
-  mode 0775
-  variables({
-            :format_opts => node.hops.nn.format_options
-        })
-end
-
-
-
-# it is ok if all namenodes format the fs. Unless you add a new one later..
-# if the nn has already been formatted, re-formatting it returns error
-# TODO: test if the NameNode is running
-if ::File.exist?("#{node.hops.home}/.nn_formatted") === false || "#{node.hops.reformat}" === "true"
-   hops_start "format-nn" do
-     action :format_nn
-   end
-else 
-  Chef::Log.info "Not formatting the NameNode. Remove this directory before formatting: (sudo rm -rf #{node.hops.nn.name_dir}/current) and set node.hops.reformat to true"
-end
-
 service_name="namenode"
 
 if node.hops.systemd == "true"
