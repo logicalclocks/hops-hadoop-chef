ea1911be4995d9a0659aa0ddd769758dbfde49d4
Theofilos Kakantousis <tkakantousis@users.noreply.github.com>
2017-07-19 15:15:53 +0200

    Copy libhopsnvml to lib native (#44)
    * copy libhopsnvml to lib/native
    
    * change to base dir
    
    * check if libhopsnvml exists
    
    * fix clause

--- recipes/default.rb
+++ recipes/default.rb
@@ -209,6 +209,17 @@ bash 'update_owner_for_gpu' do
   EOH
 end
 
+bash 'create_libhopsnvml_symlink' do
+  user "root"
+  code <<-EOH
+    set -e
+    if [ ! -f #{node.hops.base_dir}/lib/native/libhopsnvml.so ]; then
+      ln -s #{node.hops.base_dir}/share/hadoop/yarn/lib/libhopsnvml-1.0.so #{node.hops.base_dir}/lib/native/libhopsnvml.so
+    fi
+    chown #{node.hops.hdfs.user}:#{node.hops.group} #{node.hops.base_dir}/lib/native/libhopsnvml.so
+  EOH
+end
+
 template "#{node.hops.home}/etc/hadoop/yarn-env.sh" do
   source "yarn-env.sh.erb"
   owner node.hops.yarn.user
