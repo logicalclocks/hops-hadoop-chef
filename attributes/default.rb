a0d67cd96e1b54e9eade92c4d172eeb5cc69c60f
Robin Andersson <robin.eric.andersson@gmail.com>
2019-02-12 10:07:28 +0100

    set cpu to 100

--- attributes/default.rb
+++ attributes/default.rb
@@ -306,7 +306,7 @@ default['hops']['yarn']['container_executor']                = "org.apache.hadoo
 # Use Cgroup isolation
 default['hops']['yarn']['cgroups']                        = "true"
 default['hops']['yarn']['cgroups_deletion_timeout']       = "5000"
-default['hops']['yarn']['cgroups_max_cpu_usage']          = "85"
+default['hops']['yarn']['cgroups_max_cpu_usage']          = "100"
 
 #ssl-server.xml
 default['hops']['ssl']['server']['keystore']['password']   		= node['hopsworks']['master']['password']
