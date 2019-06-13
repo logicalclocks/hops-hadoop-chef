031b0885b2bda5abc02329a91c032078096c60be
Theofilos Kakantousis <t.kakantousis@gmail.com>
2019-06-13 15:20:51 +0200

    Release version: 0.10

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "jdowling@kth.se"
 license          "Apache v2.0"
 description      'Installs/Configures the Hops distribution'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.10.0"
+version          "1.0.0"
 source_url       "https://github.com/hopshadoop/hops-hadoop-chef"
 
 
