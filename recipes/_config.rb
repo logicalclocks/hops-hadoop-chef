93a42678d4c5dfe01f2c3a642e07347ce3ac8005
Jim Dowling <jdowling@sics.se>
2019-07-09 15:12:17 +0200

    updates

--- recipes/_config.rb
+++ recipes/_config.rb
@@ -5,3 +5,10 @@ if node['hops']['tls']['prod'].eql?("true")
   node.override['hops']['tls']['crl_fetcher_interval'] = "1d"
   node.override['hops']['tls']['rmappsecurity']['actor_class'] = "org.apache.hadoop.yarn.server.resourcemanager.security.HopsworksRMAppSecurityActions"
 end
+
+
+if node.attribute?(:cuda) and node["cuda"].attribute?(:accept_nvidia_download_terms)
+  if ['cuda']['accept_nvidia_download_terms'].casecmp("true") == 0
+    node.override['hops']['capacity']['resource_calculator_class'] = "org.apache.hadoop.yarn.util.resource.DominantResourceCalculatorGPU"    
+  end
+end
