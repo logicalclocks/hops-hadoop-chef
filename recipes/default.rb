30ae0fc311bece26d3b8da4feee90d5f7a6a83bb
gautier <gautier.berthou@gmail.com>
2019-11-13 16:30:57 +0100

    [HOPSWORKS-1425] change hdfs prefix to hopsfs

--- recipes/default.rb
+++ recipes/default.rb
@@ -51,7 +51,8 @@ else
   nn_https_address = private_recipe_ip("hops", "nn") + ":#{node['hops']['nn']['https_port']}"
 end
 
-defaultFS = "hdfs://#{nn_rpc_address}"
+defaultFS = "hopsfs://#{nn_rpc_address}"
+alternativeScheme = "hdfs"
 
 #
 # Constraints for Attributes - enforce them!
