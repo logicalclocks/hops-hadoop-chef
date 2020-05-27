8c67ceeb7d0a700115e9a8f5deb0795a3150fac6
robzor <robin.eric.andersson@gmail.com>
2020-05-27 17:20:20 +0200

    [HOPSWORKS-1691] APPEND fix syntax error (#218)

--- recipes/docker.rb
+++ recipes/docker.rb
@@ -8,7 +8,7 @@ when 'rhel'
 
   packages.each do |pkg|
     remote_file "#{Chef::Config['file_cache_path']}/#{pkg}" do
-      source "#{node['hops']['docker']['pkg']['download_url']['centos']/#{pkg}"
+      source "#{node['hops']['docker']['pkg']['download_url']['centos']}/#{pkg}"
       owner 'root'
       group 'root'
       mode '0755'
@@ -55,7 +55,7 @@ if node['hops']['gpu'].eql?("true")
   end
   nvidia_docker_packages.each do |pkg|
     remote_file "#{Chef::Config['file_cache_path']}/#{pkg}" do
-      source "#{node['hops']['nvidia_pkgs']['download_url']/#{pkg}"
+      source "#{node['hops']['nvidia_pkgs']['download_url']}/#{pkg}"
       owner 'root'
       group 'root'
       mode '0755'
