157796cf702dfe14b01a79bd5016137523b661a7
Fabio Buso <buso.fabio@gmail.com>
2020-06-17 18:14:20 +0200

    [HOPSWORKS-1868] Bump Hops version to RC1 in 1.3

--- recipes/_config.rb
+++ recipes/_config.rb
@@ -22,11 +22,6 @@ if node['install']['enterprise']['install'].casecmp? "true"
   node.override['hops']['schema_dir']  = "#{node['install']['enterprise']['download_url']}/hopshadoop/hops-schemas"
   node.override['dal']['download_url'] = "#{node['install']['enterprise']['download_url']}/hopshadoop/ndb-dal-#{version}-#{node['ndb']['version']}.jar"
   node.override['hops']['home']        = node['hops']['dir'] + "/hadoop-" + version
-  node.override['dal']['lib_url']      = "#{node['hops']['root_url']}/libhopsyarn-#{version}-#{node['ndb']['version']}.so"
-  node.override['nvidia']['download_url'] = "#{node['hops']['root_url']}/nvidia-management-#{version}-#{node['ndb']['version']}.jar"
-  node.override['hops']['libnvml_url']    = "#{node['hops']['root_url']}/libhopsnvml-#{version}.so"
-  node.override['amd']['download_url']    = "#{node['hops']['root_url']}/amd-management-#{version}-#{node['ndb']['version']}.jar"
-  node.override['hops']['librocm_url']    = "#{node['hops']['root_url']}/libhopsrocm-#{version}.so"
   node.override['hops']['yarn']['app_classpath'] = node['hops']['yarn']['app_classpath'].gsub(node['hops']['version'], version)
   node.override['hops']['version'] = version
   versions=node['hops']['versions']
