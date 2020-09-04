f98ebbaf846b03da26c1e1ff9688794f2ee3e5cd
Theofilos Kakantousis <t.kakantousis@gmail.com>
2020-09-04 12:33:05 +0200

    [HOPSWORKS-2007] Bump hops dependency to 3.2.0.0-RC1

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,7 +4,7 @@ include_attribute "ndb"
 include_attribute "kzookeeper"
 
 default['hops']['versions']                    = "2.8.2.2,2.8.2.3,2.8.2.4,2.8.2.5,2.8.2.6,2.8.2.7,2.8.2.8,2.8.2.9,2.8.2.10"
-default['hops']['version']                     = "3.2.0.0-SNAPSHOT"
+default['hops']['version']                     = "3.2.0.0-RC1"
 
 default['hops']['hdfs']['user']                = node['install']['user'].empty? ? "hdfs" : node['install']['user']
 default['hops']['hdfs']['user-home']           = "/home/#{node['hops']['hdfs']['user']}"
