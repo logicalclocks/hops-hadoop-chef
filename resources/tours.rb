baeae6178540f0224ade2878140aa31ae64e751c
Mahmoud Ismail <mahmoud@logicalclocks.com>
2021-03-05 17:58:22 +0100

    refactor

--- resources/tours.rb
+++ resources/tours.rb
@@ -1,16 +1,14 @@
 actions :update_local_cache
 
+# paths to copy to the local cache
 # [src,.....]
 # dst is infered from the local cache dir 
-attribute :rel_paths, :kind_of => Array, :default => []
-# [[src, dst],....]
-attribute :abs_paths, :kind_of => Array, :default => []
+attribute :paths, :kind_of => Array, :default => []
 
+# hdfs paths to which we copy the local cache paths
 # [dst,.....]
 # src is infered from the local cache dir 
-attribute :rel_tours_info, :kind_of => Array, :default => []
-#[[src,dst],...]
-attribute :abs_tours_info, :kind_of => Array, :default => []
+attribute :hdfs_paths, :kind_of => Array, :default => []
 
 attribute :owner, :kind_of => String, :default => node['hops']['hdfs']['user']
 attribute :group, :kind_of => String, :default => node['hops']['group']
