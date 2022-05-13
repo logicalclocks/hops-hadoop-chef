8226fb60485589a42dda30de949fe9999451af7b
Gibson Chikafa <gibbschikafa@gmail.com>
2022-05-13 11:19:50 +0200

    Refactor

--- recipes/docker_cgroup.rb
+++ recipes/docker_cgroup.rb
@@ -1,11 +1,12 @@
-
 docker_cgroup_cpu_cfs_quota_us = -1
 docker_cgroup_memory_hard_limit = 9223372036854771712
 docker_cgroup_memory_soft_limit = 9223372036854771712
 if node['hops']['docker']['cgroup']['enabled'].eql?("true")
   docker_cgroup_memory_hard_limit = "#{node['hops']['docker']['cgroup']['memory']['hard-limit']}"
   docker_cgroup_memory_soft_limit = "#{node['hops']['docker']['cgroup']['memory']['soft-limit']}"
-  docker_cgroup_cpu_cfs_quota_us = (node['hops']['docker']['cgroup']['cpu']["period"]) * (node['hops']['docker']['cgroup']['cpu']['quota'] / 100)
+  cpu_quota_value = node['hops']['docker']['cgroup']['cpu']['quota']
+  cpu_quota_period = node['hops']['docker']['cgroup']['cpu']["period"]
+  docker_cgroup_cpu_cfs_quota_us = (cpu_quota_period * ((cpu_quota_value).to_f / 100)).to_i
   docker_memory_cgroup_dir = "/sys/fs/cgroup/memory/docker"
   docker_cpu_cgroup_dir = "/sys/fs/cgroup/cpu/docker"
   bash "install_pkgs" do
@@ -13,7 +14,7 @@ if node['hops']['docker']['cgroup']['enabled'].eql?("true")
     group 'root'
     code <<-EOH
         echo #{docker_cgroup_cpu_cfs_quota_us} > #{docker_cpu_cgroup_dir}/cpu.cfs_quota_us
-        echo #{node['hops']['docker']['cgroup']['cpu']["period"]} > #{docker_cpu_cgroup_dir}/cpu.cfs_period_us
+        echo #{cpu_quota_period} > #{docker_cpu_cgroup_dir}/cpu.cfs_period_us
         echo #{docker_cgroup_memory_hard_limit} > #{docker_memory_cgroup_dir}/memory.limit_in_bytes
         echo #{docker_cgroup_memory_soft_limit} > #{docker_memory_cgroup_dir}/memory.soft_limit_in_bytes
     EOH
@@ -21,15 +22,14 @@ if node['hops']['docker']['cgroup']['enabled'].eql?("true")
 end
 
 docker_cgroup_rewrite_script="docker-cgroup-rewrite.sh"
-docker_cgroup_rewrite_script_path="#{node['hops']['base_dir']}/sbin}/#{docker_cgroup_rewrite_script}"
+docker_cgroup_rewrite_script_path="#{node['hops']['dir']}/sbin/#{docker_cgroup_rewrite_script}"
 template "#{docker_cgroup_rewrite_script_path}" do
   source "#{docker_cgroup_rewrite_script}.erb"
-  owner root
-  group root
+  owner "root"
+  group "root"
   mode "500"
   action :create
 end
-
 
 case node['platform_family']
 when "rhel"
@@ -43,10 +43,6 @@ template systemd_script do
   owner "root"
   group "root"
   mode 0664
-  if node['services']['enabled'] == "true"
-    notifies :enable, "service[docker-cgroup-rewrite]"
-  end
-  notifies :restart, "service[docker-cgroup-rewrite]"
   variables({
               'docker_cgroup_rewrite_script' => docker_cgroup_rewrite_script_path,
               'memory_limit_bytes' => docker_cgroup_memory_hard_limit,
@@ -58,3 +54,4 @@ end
 kagent_config "docker-cgroup-rewrite" do
   action :systemd_reload
 end
+
