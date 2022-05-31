13101bc14148e1e587b2d4e413f31e11bc7eb3a7
Salman Niazi <salman@logicalclocks.com>
2022-05-31 12:56:07 +0200

    [HOPSWORKS-3174] Bump-up RonDB version to 21.04.6 and using Hops master

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,8 +3,8 @@ include_attribute "kagent"
 include_attribute "ndb"
 include_attribute "kzookeeper"
 
-default['hops']['versions']                    = "2.8.2.2,2.8.2.3,2.8.2.4,2.8.2.5,2.8.2.6,2.8.2.7,2.8.2.8,2.8.2.9,2.8.2.10,3.2.0.0,3.2.0.1,3.2.0.2,3.2.0.3,3.2.0.4,3.2.0.5"
-default['hops']['version']                     = "3.2.0.6-RC1"
+default['hops']['versions']                    = "2.8.2.2,2.8.2.3,2.8.2.4,2.8.2.5,2.8.2.6,2.8.2.7,2.8.2.8,2.8.2.9,2.8.2.10,3.2.0.0,3.2.0.1,3.2.0.2,3.2.0.3,3.2.0.4,3.2.0.5,3.2.0.6"
+default['hops']['version']                     = "3.2.0.7-SNAPSHOT"
 default['hops']['fuse']['version']             = "1.3.2"
 
 default['hops']['hdfs']['user']                = node['install']['user'].empty? ? "hdfs" : node['install']['user']
@@ -249,7 +249,7 @@ default['mysql']['port']                    = default['ndb']['mysql_port']
 
 default['hops']['schema_dir']               = "#{node['hops']['root_url']}/hops-schemas"
 
-default['hops']['ndb']['version']           = "21.04.2"
+default['hops']['ndb']['version']           = ""
 
 if node['hops']['ndb']['version'] != ""
   node.override['ndb']['version'] = node['hops']['ndb']['version']
