10f647de3440b6f7b3e4af94a6ba0fd2fe837164
Fabio Buso <buso.fabio@gmail.com>
2022-11-04 15:51:22 +0100

    HOPSFS-20: Added support for AWS end point and AWS credentials (#388) (#406)
    * [HOPS-EE-141] Added support for AWS end point and AWS credentials
    
    * HOPSFS-20: AWS Endpoint support. Updates according to review
    
    * HOPSFS-20: AWS Endpoint support. Updates according to review
    
    Co-authored-by: Salman Niazi <salman@logicalclocks.com>

--- recipes/default.rb
+++ recipes/default.rb
@@ -407,3 +407,21 @@ cookbook_file "#{node['hops']['conf_dir']}/namenode.yaml" do
   group node['hops']['group']
   mode 500
 end
+
+hdfs_user_home = conda_helpers.get_user_home(node['hops']['hdfs']['user'])
+if not node['hops']['aws_access_key_id'].eql?("") and not node['hops']['aws_secret_access_key'].eql?("")
+  directory "#{hdfs_user_home}/.aws" do
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    mode "0755"
+    action :create
+  end
+  
+  template "#{hdfs_user_home}/.aws/credentials" do
+    source "credentials.erb"
+    owner node['hops']['hdfs']['user']
+    group node['hops']['group']
+    mode "600"
+    action :create
+  end
+end
