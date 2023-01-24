32e02ec6aed832e20d268a15ae07400c605da159
Jim Dowling <jim@logicalclocks.com>
2023-01-24 23:05:35 +0100

    refactor out hdfs user/crypto to a reusable recipe for flyingduck

---
+++ recipes/hdfs_user.rb
@@ -0,0 +1,19 @@
+
+
+group node['hops']['secure_group'] do
+  action :modify
+  members node['hops']['hdfs']['user']
+  append true
+  not_if { node['install']['external_users'].casecmp("true") == 0 }
+end
+
+crypto_dir = x509_helper.get_crypto_dir(node['hops']['hdfs']['user'])
+kagent_hopsify "Generate x.509" do
+  user node['hops']['hdfs']['user']
+  crypto_directory crypto_dir
+  common_name "flyingduck.service.consul"
+  action :generate_x509
+  not_if { node["kagent"]["enabled"] == "false" }
+end
+
+
--- recipes/nn.rb
+++ recipes/nn.rb
@@ -4,20 +4,7 @@ template_ssl_server()
 
 my_ip = my_private_ip()
 
-group node['hops']['secure_group'] do
-  action :modify
-  members node['hops']['hdfs']['user']
-  append true
-  not_if { node['install']['external_users'].casecmp("true") == 0 }
-end
-
-crypto_dir = x509_helper.get_crypto_dir(node['hops']['hdfs']['user'])
-kagent_hopsify "Generate x.509" do
-  user node['hops']['hdfs']['user']
-  crypto_directory crypto_dir
-  action :generate_x509
-  not_if { node["kagent"]["enabled"] == "false" }
-end
+include_recipe "hops::hdfs_user"
 
 file "#{node['hops']['conf_dir']}/dfs.exclude" do 
   owner node['hops']['hdfs']['user']
