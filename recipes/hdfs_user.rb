e096d2e6bcfc826c2dc72aa215095836209545b6
Jim Dowling <jim@logicalclocks.com>
2023-01-25 16:20:43 +0100

    Revert "support multiple NNs - prepend namenode fqdn with instance_id"
    This reverts commit 1e40290e75f5ea7661ac2289911d05a30614c9f0.

--- recipes/hdfs_user.rb
+++ recipes/hdfs_user.rb
@@ -6,14 +6,11 @@ group node['hops']['secure_group'] do
   not_if { node['install']['external_users'].casecmp("true") == 0 }
 end
 
-instance_id = private_recipe_ips("hops", "nn").sort.find_index(my_private_ip())
-fqdn = consul_helper.get_service_fqdn("namenode")
-
 crypto_dir = x509_helper.get_crypto_dir(node['hops']['hdfs']['user'])
 kagent_hopsify "Generate x.509" do
   user node['hops']['hdfs']['user']
   crypto_directory crypto_dir
-  common_name "#{instance_id}.#{fqdn}"
+  common_name "namenode.service.consul"
   action :generate_x509
   not_if { node["kagent"]["enabled"] == "false" }
 end
