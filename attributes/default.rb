f0e5d43e33b2ef6cd0faabf38c541476ac7f2607
Salman Niazi <salman@logicalclocks.com>
2023-05-16 15:11:10 +0200

    RONDB-384: Bump RonDB version to 21.04.13

--- attributes/default.rb
+++ attributes/default.rb
@@ -259,7 +259,7 @@ default['mysql']['port']                    = default['ndb']['mysql_port']
 
 default['hops']['schema_dir']               = "#{node['hops']['root_url']}/hops-schemas"
 
-default['hops']['ndb']['version']           = "21.04.12"
+default['hops']['ndb']['version']           = "21.04.13-SNAPSHOT"
 
 if node['hops']['ndb']['version'] != ""
   node.override['ndb']['version'] = node['hops']['ndb']['version']
