a033cdee7dde674a6e62c1341a35828d60b3a558
Salman Niazi <salman@logicalclocks.com>
2023-08-28 21:05:03 +0200

    RONDB-458: Bump RonDB Version to 21.04.15 (#478)
    * RONDB-458: Bump RonDB Version to 21.04.15
    
    * RONDB-458: Update HopsFS Version to 3.2.0.11-SNAPSHOT

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,9 +3,9 @@ include_attribute "kagent"
 include_attribute "ndb"
 include_attribute "kzookeeper"
 
-default['hops']['versions']                    = "2.8.2.2,2.8.2.3,2.8.2.4,2.8.2.5,2.8.2.6,2.8.2.7,2.8.2.8,2.8.2.9,2.8.2.10,3.2.0.0,3.2.0.1,3.2.0.2,3.2.0.3,3.2.0.4,3.2.0.5,3.2.0.6,3.2.0.7,3.2.0.8,3.2.0.9"
-default['hops']['version']                     = "3.2.0.10-RC0"
-default['hops']['fuse']['version']             = "3.2.0.10"
+default['hops']['versions']                    = "2.8.2.2,2.8.2.3,2.8.2.4,2.8.2.5,2.8.2.6,2.8.2.7,2.8.2.8,2.8.2.9,2.8.2.10,3.2.0.0,3.2.0.1,3.2.0.2,3.2.0.3,3.2.0.4,3.2.0.5,3.2.0.6,3.2.0.7,3.2.0.8,3.2.0.9,3.2.0.10"
+default['hops']['version']                     = "3.2.0.11-SNAPSHOT"
+default['hops']['fuse']['version']             = "3.2.0.11"
 
 default['hops']['hdfs']['user']                = node['install']['user'].empty? ? "hdfs" : node['install']['user']
 default['hops']['hdfs']['user_id']             = '1506'
@@ -268,7 +268,7 @@ default['mysql']['port']                    = default['ndb']['mysql_port']
 
 default['hops']['schema_dir']               = "#{node['hops']['root_url']}/hops-schemas"
 
-default['hops']['ndb']['version']           = "21.04.14"
+default['hops']['ndb']['version']           = "21.04.15"
 
 if node['hops']['ndb']['version'] != ""
   node.override['ndb']['version'] = node['hops']['ndb']['version']
