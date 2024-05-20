f29e05cfa0544e8f16d889aa57b1bd3a66dad403
Salman Niazi <salman@logicalclocks.com>
2024-05-20 09:39:00 +0200

    RONDB-661 RonDB minor release 22.10.3

--- attributes/default.rb
+++ attributes/default.rb
@@ -259,7 +259,7 @@ default['mysql']['port']                    = default['ndb']['mysql_port']
 
 default['hops']['schema_dir']               = "#{node['hops']['root_url']}/hops-schemas"
 
-default['hops']['ndb']['version']           = "22.10.1"
+default['hops']['ndb']['version']           = "22.10.3"
 
 if node['hops']['ndb']['version'] != ""
   node.override['ndb']['version'] = node['hops']['ndb']['version']
